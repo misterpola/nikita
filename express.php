c936b0e20d56902ff22243bdcf13a782fb02a3c1
Gonzalo Bellanti <misterpola@gmail.com>
2018-02-06 20:51:12 -0300

    big changes

--- express.php
+++ express.php
@@ -13,7 +13,23 @@
 <!-- MAKE BODY FADE IN -->
 <body class="fade-in">
 
-  <?php include 'header.php' ?>
+  <nav class="navbar navbar-expand-xs navbar-dark fixed-top">
+      <a class="navbar-brand" href="#">
+        <img class="large" id="logo" src="img/logo.png" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  </nav>
+  <div class="collapse navbar-collapse" id="nav">
+      <div class="">
+        <div class="full navbar-nav text-center d-flex flex-column align-items-center justify-content-center">
+          <a class="nav-item nav-link text-white" data-target="capture.php" href="#">CAPTURE</a>
+          <a class="nav-item nav-link text-white" data-target="express.php" href="#">EXPRESS</a>
+          <a class="nav-item nav-link text-white" data-target="contact.php" href="#">CONTACT</a>
+        </div>
+      </div>
+  </div>
 
   <!-- BACKGROUND PICTURE -->
   <div class="container-fluid landing landing-express d-flex align-items-end justify-content-center">
